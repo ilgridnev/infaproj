b398a7085c2529e4d1548c4298f94940d11ac5fc
Илья <gridnev.io@phystech.edu>
2021-11-30 12:06:00 +0300

    j

---
+++ maincode.py
@@ -0,0 +1,8 @@
+
+# инициализация экрана, настройка частоты обновления, подключение библиотек
+import pygame
+from random import randint
+
+pygame.init()
+FPS = 25
+screen = pygame.display.set_mode((1000, 600))
