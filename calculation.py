8286ca79062d356ef8601c72faaa207b40aeaf62
Katancss <tretiakov.iua@phystech.edu>
2021-12-11 16:48:07 +0300

    release solving systems

--- calculation.py
+++ calculation.py
@@ -1,59 +1,82 @@
 import networkx as nx
+import numpy as np
 
 
-class Contours:
-    def __init__(self, adjacency_matrix):
-        """
+adjacency_matrix = [[0, 2, 0, 1, 0, 0, 0, 0, 0],
+                    [2, 0, 0, 0, 1, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+                    [1, 0, 0, 0, 0, 0, 2, 0, 0],
+                    [0, 1, 0, 0, 0, 2, 0, 2, 0],
+                    [0, 0, 0, 0, 2, 0, 0, 0, 2],
+                    [0, 0, 0, 2, 0, 0, 0, 2, 0],
+                    [0, 0, 0, 0, 2, 0, 2, 0, 1],
+                    [0, 0, 0, 0, 0, 2, 0, 1, 0]]
 
-        Этот класс выделяет в цепи контуры, на вход принимается матрица смежности,
-         которая полность описывает электрическую цепь
-        """
-        self.adjacency_matrix = adjacency_matrix
-
-        # создаем объект граф
-        self.graph = nx.Graph()
-
-        # ассоциируем вершины графа с матрицей смежности
-        self.graph.add_edges_from([(i + 1, j + 1) for i in range(len(adjacency_matrix))
-                                   for j in range(len(adjacency_matrix)) if adjacency_matrix[i][j] != 0])
-
-        # находим все базисные циклы в графе
-        self.cycles = list(nx.cycle_basis(self.graph))
-
-        # добавляем в конец цикла его начальную вершину для удобства расчета
-        for i in range(len(self.cycles)):
-            self.cycles[i].append(self.cycles[i][0])
-
-        # создаем и заполняем список списков с парами вершин и элементом между ними
-        self.element_pairs = []
-        for i in range(len(self.cycles)):
-            sub = []
-            for j in range(len(self.cycles[i])):
-                if j + 1 < len(self.cycles[i]):
-                    sub.append(
-                        (self.cycles[i][j], self.cycles[i][j + 1],
-                         self.adjacency_matrix[self.cycles[i][j] - 1][self.cycles[i][j + 1] - 1]))
-            self.element_pairs.append(sub)
-
-    def get_contour(self, number):
-        """
-
-        принимает итерируемый параметр number, который отвечает за номер контура
-        возвращает контур с таким номером
-        """
-        return self.element_pairs[number]
+"""
+adjacency_matrix = [[0, 2, 0, 2],
+                    [2, 0, 1, 0],
+                    [0, 1, 0, 2],
+                    [2, 0, 2, 0]]
+"""
 
 
-# создаем матрицу смежности
-adjacency_matrix1 = [[0, 1, 0, 1, 0, 0, 0, 0, 0],
-                     [1, 0, 0, 0, 2, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [1, 0, 0, 0, 3, 0, 0, 0, 0],
-                     [0, 2, 0, 3, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+def replace(arr, a, b):
+    for i in range(len(arr)):
+        if arr[i][0] == a:
+            arr[i][0] = b
+        elif arr[i][1] == a:
+            arr[i][1] = b
 
-contour = Contours(adjacency_matrix1)
-print(contour.get_contour(0))
+edges_0 = []
+equal = []
+edges = []
+for i in range(len(adjacency_matrix)):
+    for j in range(len(adjacency_matrix)):
+        if adjacency_matrix[i][j] != 0:
+            if i + 1 < j + 1:
+                edges_0.append([i + 1, j + 1])
+                if adjacency_matrix[i][j] == 1:
+                    equal.append([i + 1, j + 1])
+
+for i in range(len(equal)):
+    for j in range(len(edges_0)):
+        replace(edges_0, max(equal[i][0], equal[i][1]), min(equal[i][0], equal[i][1]))
+
+for i in range(len(edges_0)):
+    if edges_0[i][0] != edges_0[i][1]:
+        edges.append(edges_0[i])
+
+G = nx.Graph()
+G.add_edges_from(edges)
+
+nodes = list(G.nodes)
+q = len(nodes)
+p = len(edges)
+
+A = np.zeros(shape=(q-1, p))
+for i in range(q - 1):
+    for j in range(p):
+        if edges[j][0] == nodes[i]:
+            A[i][j] = 1
+        elif edges[j][1] == nodes[i]:
+            A[i][j] = -1
+A_T = A.transpose()
+Y = np.zeros(shape=(p, p))
+
+for i in range(p):
+    for j in range(p):
+        if i == j:
+            Y[i][j] = 0.5
+
+E = np.array([[0], [1], [0], [0], [0], [0]])
+
+Left_side = A.dot(Y)
+Left_side = Left_side.dot(A_T)
+
+Right_side = -A.dot(Y)
+Right_side = Right_side.dot(E)
+
+U_0 = np.linalg.solve(Left_side, Right_side)
+print(U_0)
+print(edges)
+
