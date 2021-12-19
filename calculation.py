26e444aa2a46aea4f50ffbafa28a67ec4f8a4deb
Katancss <tretiakov.iua@phystech.edu>
2021-12-19 20:04:36 +0300

    Исправлены ошибки, возникающие из-за нажатия на узлы не присутсвующие в схеме

--- calculation.py
+++ calculation.py
@@ -4,15 +4,15 @@ import numpy as np
 
 class Calculation:
     """
-    Данный класc производит просчёт электрической цепи методом узловых потенциалов:
+    Данный класс производит просчёт электрической цепи методом узловых потенциалов:
     Каждой вершине планарного графа цепи сопостовляется свой потенциал. Затем составляется и решается СЛУ
     """
+
     def __init__(self):
         """
         Инициализация необходимых для описания поля потенциалов списков
         """
         self.U_0 = []
-        self.equal = []
         self.edges = []
         self.nodes = []
 
@@ -26,22 +26,15 @@ class Calculation:
         где U_0 вектор потенциалов в узлах. Решаем полученную систему методом numpy.linalg.
         """
 
-        def replace(arr, a, b):
-            """
-            вспомогательная функция меняющая все элементы a на b в списке arr
-            """
-            for i in range(len(arr)):
-                if arr[i][0] == a:
-                    arr[i][0] = b
-                elif arr[i][1] == a:
-                    arr[i][1] = b
-
+        # пересчитываем матрицу смежности в список смежности
         self.edges = []
         for i in range(len(adjacency_matrix)):
             for j in range(len(adjacency_matrix)):
                 if adjacency_matrix[i][j] != 0:
                     if i + 1 < j + 1:
                         self.edges.append([i + 1, j + 1])
+
+        # создаем объект класса Graph, который облегчает работу с планарным графом цепи
         G = nx.Graph()
         G.add_edges_from(self.edges)
 
@@ -49,7 +42,12 @@ class Calculation:
         q = len(self.nodes)
         p = len(self.edges)
 
-        A = np.zeros(shape=(q - 1, p))
+        # заполняем необходимые для вычислений матрицы
+        if q - 1 >= 0:
+            A = np.zeros(shape=(q - 1, p))
+        else:
+            A = np.zeros(shape=(q, p))
+
         for i in range(q - 1):
             for j in range(p):
                 if self.edges[j][0] == self.nodes[i]:
@@ -66,18 +64,22 @@ class Calculation:
                 Y[i][i] = 0.5
             elif adjacency_matrix[self.edges[i][0] - 1][self.edges[i][1] - 1] == 3:
                 Y[i][i] = 100000
+            elif adjacency_matrix[self.edges[i][0] - 1][self.edges[i][1] - 1] == 4:
+                Y[i][i] = 0.2
 
         E = np.zeros(shape=(p, 1))
         for i in range(p):
             if adjacency_matrix[self.edges[i][0] - 1][self.edges[i][1] - 1] == 3:
                 E[i][0] = 1
 
+        # составляем матричное уравнение
         Left_side = A.dot(Y)
         Left_side = Left_side.dot(A_T)
 
         Right_side = -A.dot(Y)
         Right_side = Right_side.dot(E)
 
+        # решаем систему уравнений с помощью метода нахождения обратной матрицы
         self.U_0 = np.linalg.solve(Left_side, Right_side)
         self.U_0 = np.append(self.U_0, [0])
 
@@ -88,5 +90,8 @@ class Calculation:
 
         возвращает напряжение (абсолютную разность потенциалов)
         """
-        res = abs(self.U_0[self.nodes.index(node1)] - self.U_0[self.nodes.index(node2)])
-        return round(res, 2)
+        if node1 in self.nodes and node2 in self.nodes:
+            res = abs(self.U_0[self.nodes.index(node1)] - self.U_0[self.nodes.index(node2)])
+            return round(res, 2)
+        else:
+            return 0.0
